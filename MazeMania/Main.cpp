53617687d0a9ceb9b2fed055306609b3175eae0d
jjamesdk <jjamesdk@yahoo.com>
2014-08-03 17:22:47 -0400

    Revert "changes"
    This reverts commit 1f50b709980cae82558898b0dbffabc8b84d9d85.

--- MazeMania/Main.cpp
+++ MazeMania/Main.cpp
@@ -1,21 +1,15 @@
-// James Kirsch - Maze Project
-// Open Source, Play around with the code
-
 #include <iostream>
 
 #include "MazeMania.Data.h"
-#include "MazeMania.Logic.h"
-#include "MazeMania.Object.h"
 
 using namespace std;
 using namespace MazeManiaData;
-using namespace MazeManiaLogic;
-using namespace MazeManiaObject;
 
 // MazeMania by James Kirsch
 
 void main()
 {
+	
 	int response;
 	cout << "Test" << endl;
 	cin >> response; cout << endl;

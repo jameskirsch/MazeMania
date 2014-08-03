9d4ef1ea441c22443a02d9969c53f49a06888020
jjamesdk <jjamesdk@yahoo.com>
2014-08-03 17:50:56 -0400

    Added Logic Project

--- MazeMania/Main.cpp
+++ MazeMania/Main.cpp
@@ -2,17 +2,18 @@
 
 #include "MazeMania.Data.h"
 #include "MazeMania.Object.h"
+#include "MazeMania.Logic.h"
 
 using namespace std;
 using namespace MazeManiaData;
 using namespace MazeManiaObject;
+using namespace MazeManiaLogic;
 
 
 // MazeMania by James Kirsch
 
 void main()
-{
-	
+{	
 	int response;
 	cout << "Test" << endl;
 	cin >> response; cout << endl;

c08febb440a68a56965d81df4aeb7a681b5b56cf
jjamesdk <jjamesdk@yahoo.com>
2015-01-19 18:40:38 -0500

    sc

--- MazeMania.Object/Entity.h
+++ MazeMania.Object/Entity.h
@@ -4,12 +4,28 @@
 
 namespace MazeManiaObject
 {
+
+	// Generic Class for many objects to inherit off of.
 	class __declspec(dllexport) Entity{
 
 	public:
 
 		void SetId(int id);
 		
+		int GetId();
+
+	protected:
+
+		int m_Id;
+	};
+
+
+	/// Grid Class - Used to any Entities on Map
+	class __declspec(dllexport) Grid{
+
+	public:
+
+		void SetId(int id);
 		int GetId();
 
 	protected:

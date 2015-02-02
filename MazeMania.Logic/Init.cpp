76301b2a1acfcc9315afdcf22ff0f623002c5403
jjamesdk <jjamesdk@yahoo.com>
2015-02-02 03:02:24 -0500

    Resized Vector appropriately

--- MazeMania.Logic/Init.cpp
+++ MazeMania.Logic/Init.cpp
@@ -4,32 +4,19 @@ namespace MazeManiaLogic {
 
 	//Initialize all Game Features
 	bool Game::Init() {
-	
-		//This should Create an amount of Maps, and push them into the container : TO DO: Fix memory issues
-		int numMaps = 3;
 
-		for (int i = 0; i <= numMaps; i++) {
-			auto p = m_Map.get();
-			Map* m = new Map[i];
-
-			for (auto &i : *this->m_Map) {
-				p->push_back(m[i]); // problem spot
-			}
-		}
-
-		//For testing
-		std::cout << "Size: " << p->size() << std::endl;
-
-		//Set the Position of Maps on the Game World, Set Any other Map Properties, such as Color, Size, etc.
-		float x = 0.f, y = 0.f;
-		std::for_each(this->GetMapCollection().begin(), this->GetMapCollection().end(),[&x, &y](Map& m) { 
+		//TO DO: Make this value an input
+		int numMaps = 7;
+		
+		//Size the Map Container based on input and Set Map Properties
+		m_Map->resize(numMaps);
+		float x = 0.f, y = 0.f; // Map Position
+		std::for_each(GetMapCollection().begin(), GetMapCollection().end(),[&x, &y](Map& m) { 
 			m.setSize(sf::Vector2f(50.f, 50.f)),
-			m.setFillColor(sf::Color::Green),
+			m.setFillColor(sf::Color::Red),
 			m.setPosition( x, y),
 			x += 50.f, y += 50;
 		});
-
-		//std::cout << "Size Of Map Container: " << p->size() << std::endl;
 
 		return true;
 	}
